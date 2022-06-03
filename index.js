7a9fd46307cade021f7c7caf2a2f9f78efd4c4d8
Shaik Sohail Hussain <shaiksohailhu7n@gmail.com>
2022-06-04 00:51:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,9 +71,7 @@ function makeSound(key) {
 function buttonAnimation(currentKey) {
 
   var activeButton = document.querySelector("." + currentKey);
-
   activeButton.classList.add("pressed");
-
   setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
